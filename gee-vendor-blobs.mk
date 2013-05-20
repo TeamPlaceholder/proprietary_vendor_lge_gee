975dbca8596b355fd4055f94c562a912476bb705
xboxfanj <xboxfanj@msn.com>
2013-05-20 18:21:14 -0500

    Build more from source
    Change-Id: Iadb9cbb0db7e0695b57dba5137e9145ca5b38b71

--- gee-vendor-blobs.mk
+++ gee-vendor-blobs.mk
@@ -126,14 +126,12 @@ PRODUCT_COPY_FILES += \
     vendor/lge/gee/proprietary/lib/libDxHdcp.so:system/lib/libDxHdcp.so \
     vendor/lge/gee/proprietary/lib/libgemini.so:system/lib/libgemini.so \
     vendor/lge/gee/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/lge/gee/proprietary/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \
     vendor/lge/gee/proprietary/lib/libidl.so:system/lib/libidl.so \
     vendor/lge/gee/proprietary/lib/libimage-jpeg-dec-omx-comp.so:system/lib/libimage-jpeg-dec-omx-comp.so \
     vendor/lge/gee/proprietary/lib/libimage-jpeg-enc-omx-comp.so:system/lib/libimage-jpeg-enc-omx-comp.so \
     vendor/lge/gee/proprietary/lib/libimage-omx-common.so:system/lib/libimage-omx-common.so \
     vendor/lge/gee/proprietary/lib/libllvm-a3xx.so:system/lib/libllvm-a3xx.so \
     vendor/lge/gee/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
-    vendor/lge/gee/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
     vendor/lge/gee/proprietary/lib/libmercury.so:system/lib/libmercury.so \
     vendor/lge/gee/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
     vendor/lge/gee/proprietary/lib/libmmcamera_faceproc.so:system/lib/libmmcamera_faceproc.so \
